d58f0638743a69d65f0db2523a0ad6903cdc505f
Jinyuan Zhang <823573476@qq.com>
2025-03-28 23:28:38 +0800

    update on parallel imaging

--- plot/plot_PE_PI.m
+++ plot/plot_PE_PI.m
@@ -17,11 +17,19 @@ function [fig] = plot_PE_PI(R, nX, nY, pe_Img, pe_Ref, pe_ImgAndRef, pe_full)
     title(sprintf('R = %d, nAcq = %d, nRef = %d', R, length(pe_Img)+length(pe_Ref), ...
         length(pe_Ref)+length(pe_ImgAndRef)), 'FontWeight', 'bold', 'Color', 'r');
 
-    impixelinfo;
+    % impixelinfo;
     colormap(cmap);          
     clim([0, 4]);
     cb = colorbar;   
     cb.Ticks = (1:(5-1))+0.5; 
     cb.TickLabels = ["Img", "Ref", "Img & Ref"];         
     cb.TickLength = 0;
+    cb.Color = '#CCCCCC';             
+    cb.FontSize = 10;  
+
+    ax = gca;
+    ax.Position = [0. 0.03 0.80 0.9];      
+    ax.LooseInset = [0 0 0 0]; 
+    set(fig, 'Color', '#1F1F1F');  
+    set(fig, 'InvertHardcopy', 'off');  
 end
--- prep_PEOrder_PI.m
+++ prep_PEOrder_PI.m
@@ -1,7 +1,6 @@
-% function [PEorder, PElabel, phaseAreas, nExcit, pe_full, pe_Img, pe_Ref, pe_ImgAndRef] = prep_PEOrder_PI(PEMode, nY, nEcho, TEeff, TE1, deltak, R, RefLinesRatio)
-function [PEorder, PElabel, phaseAreas, nExcit, pe_full, pe_Img, ...
-          pe_Ref, pe_ImgAndRef] = prep_PEOrder_PI(PEMode, nY, nEcho, TEeff, ...
-          TE1, deltak, R, RefLinesRatio)
+function [PEorder, PElabel, phaseAreas, nAcq, nRef, nExcit, pe_full, pe_Img, ...
+          pe_Ref, pe_ImgAndRef, kSpaceCenterLine, FirstRefLine] = prep_PEOrder_PI(PEMode, ...
+          nY, nEcho, TEeff, TE1, deltak, R, RefLinesRatio)
     
     if R == 1
         nExcit  = floor(nY / nEcho);
@@ -13,17 +12,18 @@ function [PEorder, PElabel, phaseAreas, nExcit, pe_full, pe_Img, ...
     elseif R > 1 % for parallel imaging
         % 1. determine nRef (to make mod(nRef+nImg, nEcho) == 0)
         pe_full   = (1:(nY)) - floor(0.5 * nY) - 1;
+        
         nImg      = round(nY/R);
         nRef      = round(nY * RefLinesRatio * (R-1)/R);
         nRef      = round((nImg+nRef) / nEcho) * nEcho - nImg; 
 
         % 2. Img
-        pe_Img    = pe_full(1:R:end); 
+        pe_Img    = fliplr(pe_full(end:-R:1)); 
         pe_Img    = pe_Img(1:nImg);
 
         % 3. Ref
         pe_Ref    = pe_full;          
-        pe_Ref(1:R:end) = [];
+        pe_Ref(end:-R:1) = [];
         n         = length(pe_Ref);
         mid_start = floor((n - nRef) / 2) + 1; 
         mid_end   = mid_start + nRef - 1;       
@@ -37,6 +37,7 @@ function [PEorder, PElabel, phaseAreas, nExcit, pe_full, pe_Img, ...
         pe_steps  = sort([pe_Img pe_Ref], "ascend");
     end
     nAcq    = nExcit * nEcho;
+    nRef    = length(pe_Ref) + length(pe_ImgAndRef);
 
     k0prescr = max(round(TEeff/TE1), 1); % echo to be aligned to the k-space center 
     k0curr   = 1;
@@ -63,6 +64,19 @@ function [PEorder, PElabel, phaseAreas, nExcit, pe_full, pe_Img, ...
             error('Invalid PEMode');
     end
     PEorder    = circshift(pe_steps, k0prescr - k0curr);
-    PElabel    = PEorder - min(PEorder(:));
     phaseAreas = PEorder * deltak;
+    PElabel    = PEorder - min(PEorder(:));
+
+    if R > 1
+        PElabel = PElabel + (nY-1-max(PElabel(:)));
+    end
+
+    [row, col] = find(PEorder == 0);
+    kSpaceCenterLine = PElabel(row, col);
+    if R > 1
+        [row, col] = find(PEorder == min(union(pe_ImgAndRef, pe_Ref)));
+        FirstRefLine = PElabel(row, col);
+    else
+        FirstRefLine = -1;
+    end
 end
